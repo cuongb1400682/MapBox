f6defbae0487fc49043aa6241b55597173e6fd2c
Nguyễn Tấn Cường <tancuong2596@gmail.com>
2017-06-30 10:01:27 +0700

    Add access token

--- app/src/main/java/cit/edu/mapbox/MainActivity.java
+++ app/src/main/java/cit/edu/mapbox/MainActivity.java
@@ -3,11 +3,14 @@ package cit.edu.mapbox;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mapbox.mapboxsdk.Mapbox;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.access_token));
         setContentView(R.layout.activity_main);
     }
 }
