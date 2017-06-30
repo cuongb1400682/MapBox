045516ead7a4f4c45be3da822cf5b73fe3ad9ede
Nguyễn Tấn Cường <tancuong2596@gmail.com>
2017-06-30 10:08:37 +0700

    Add MapView to layout file

--- app/src/main/java/cit/edu/mapbox/MainActivity.java
+++ app/src/main/java/cit/edu/mapbox/MainActivity.java
@@ -4,13 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Mapbox.getInstance(this, getString(R.string.access_token));
         setContentView(R.layout.activity_main);
+
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
     }
 }
